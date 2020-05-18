f1f11ea6a75b4492c964bc7fa79a48229487f380
Marius Birk <mbirk1@users.noreply.github.com>
2020-05-18 18:06:46 +0200

    Marius überarbeitet

--- Übung4/von Marius/Übung4/src/Functions.java
+++ Übung4/von Marius/Übung4/src/Functions.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+//Dieser Code läuft für das explizite Beispiel aus der Aufgabe.
 public class Functions {
     public static void main(String[]  args){
         double i = 2;
@@ -11,22 +13,26 @@ interface Function{
     double  result=0;
 }
 class SineFunction implements Function{
-    private boolean square=true;
+    private String function =" ";
     public SineFunction(SquareFunction squareFunction) {
-        if(squareFunction.){
-            square=true;
+        if(squareFunction.getClass()== SquareFunction.class){
+            function="square";
         }
         else{
-            square=false;
+            function="sine";
         }
+        //Diese If Abfragen können bei beliebiger Reihenfolge der Funktionen entsprechend erweitert werden.
+        //Weitere Anpassungen am Code sind dann aber noch nötig.
     }
     @Override public void calculate(double i ){
-
+        if(function.equals("sqaure")){
+            i=i*i;
+        }
+        i=Math.sin(i);
+        System.out.println(i);
     }
 }
 class SquareFunction implements Function{
-
-
     @Override
     public void calculate(double i) {
 
